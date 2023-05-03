b54e094b72ff4bb1eba72b2deb072da0bcbbbdda
gllisboa <guilhermeluzlisboa@gmail.com>
2023-05-04 00:52:40 +0100

    Manutencao feita

--- alunos.js
+++ alunos.js
@@ -3,7 +3,7 @@ const frequenciaMinimaAprovacao = 80
 
 function validaAprovacao(media, frequencia) {
 
-    if ((media >= mediaMinimaAprovacao) && (frequencia >= frequenciaMinimaAprovacao )) {
+    if ((media >= mediaMinimaAprovacao) && (frequencia > frequenciaMinimaAprovacao )) {
         return true
     }else {
         return false
